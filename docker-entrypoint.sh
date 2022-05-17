9f30a9e91ff701cb2dcb38794b127db3b0709bb1
chenxd <690115879@qq.com>
2022-05-17 17:11:01 +0800

    dbpath从环境变量中读取

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export DB_PATH=/data.db
+
 echo "$TZ" >  /etc/timezone
 cp /usr/share/zoneinfo/$TZ   /etc/localtime
 exec "$@"
--- pkg/orm/orm.go
+++ pkg/orm/orm.go
@@ -3,6 +3,7 @@ package orm
 import (
 	"db_agent/pkg/global"
 	"fmt"
+	"os"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -10,7 +11,11 @@ import (
 
 func init() {
 	var err error
-	global.DataDb, err = gorm.Open(sqlite.Open("./data/data.db"), &gorm.Config{})
+	path := os.Getenv("DB_PATH")
+	if path == "" {
+		path = "./data/data.db"
+	}
+	global.DataDb, err = gorm.Open(sqlite.Open(path), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
 		panic("Failed to connect db")
